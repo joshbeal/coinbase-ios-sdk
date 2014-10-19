1ed714e90e8ff8d6a858f5953af9f6c5397b03a6
Josh Beal <jbeal24@gmail.com>
2014-10-18 19:51:15 -0700

    add missing view controller

---
+++ Classes/CBAuthorizationViewController.m
@@ -0,0 +1,57 @@
+//
+//  CBAuthorizationViewController.m
+//  Pods
+//
+//  Created by Josh Beal on 10/18/14.
+//
+//
+
+#import "CBAuthorizationViewController.h"
+#import "Coinbase.h"
+
+@interface CBAuthorizationViewController () <UIWebViewDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
+
+@end
+
+@implementation CBAuthorizationViewController
+
+- (instancetype)initWithURL:(NSURL *)authURL {
+    self = [super init];
+    if (self) {
+        _webView = [[UIWebView alloc] init];
+        _webView.delegate = self;
+        [_webView loadRequest:[NSURLRequest requestWithURL:authURL]];
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.view addSubview:self.webView];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    CGRect b = self.view.bounds;
+    
+    self.webView.frame = CGRectMake(0, 0, CGRectGetWidth(b), CGRectGetHeight(b));
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    NSString *title=[webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    if (![title isEqualToString:@"Coinbase"]) {
+        [Coinbase registerAuthCode:title];
+        [self dismissViewControllerAnimated:NO completion:nil];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
